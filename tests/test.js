9ff37126799cbe50ef2f93bd11c21b776b62922b
SERKAN BIRCAN <serkanbircan21@outlook.com>
2019-06-23 21:04:01 +0300

    Revert "added: GetGameInfo"
    This reverts commit b2463ce4203b7056e45bc2a53c1c0638d6267d82.

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,7 @@ const PrintUserData = async () =>
 {
     try
     {
-        const user = await Krunker.GetProfile("fasetto");
+        const user = await Krunker.GetProfile("needmoney90");
         console.log(user);
     }
     catch (e)
@@ -32,50 +32,5 @@ const PrintLeaderboard = async () =>
     }
 }
 
-const PrintGameInfo = async () =>
-{
-    try
-    {
-        const gameInfo = await Krunker.GetGameInfo("FRA:piucd");
-        console.log(gameInfo);
-
-    }
-    catch (e)
-    {
-        console.log(e.message);
-    }
-}
-
-PrintUserData();
-// PrintLeaderboard();
-// PrintGameInfo();
-
-// const { decode } =  require('msgpack-lite');
-
-// const fromHexString = hexString => new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-
-// const decoded = decode(fromHexString("92a17294a76c656164657273a573636f7265a6313234343033c00d01"));
-
-// console.log(decoded);
-
-// Normal
-// { player_name: 'esmmotario',
-//     player_featured: 0,
-//     player_clan: 'boda',
-//     player_score: 5714125,
-//     player_hack: 0 }
-
-// Krunkies
-// { player_name: 'JHXC',
-// player_featured: 1,
-// player_clan: '',
-// player_funds: 22196,
-// player_hack: 0 } ]
-
-// Clans
-// { clan_name: 'hi',
-// clan_score: 16040555,
-// creatorname: 'superman2505',
-// player_clan: 'hi',
-// clan_membercount: 93,
-// clan_hackcount: 2 }
+// PrintUserData();
+PrintLeaderboard();
